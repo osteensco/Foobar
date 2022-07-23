1e15a8aaac640ea5619e3198aefbed04b520e63a
br0steen <osteensco@gmail.com>
2022-07-23 18:16:58 -0400

    rnd 2 solution 2 - first try worked!

--- rnd2.py
+++ rnd2.py
@@ -24,3 +24,42 @@ def solution(x,y):
     #normalize:(x + (y-1)) * ((x-1) + (y-1))/2 + x
 
     return str(x + ((x+y-1) * (x+y-2)) / 2)
+
+
+def solution2(string):
+    '''
+    Write a program that counts how many salutes are exchanged during a typical walk along a hallway. 
+    The hall is represented by a string. For example:
+"--->-><-><-->-"
+
+Each hallway string will contain three different types of characters: '>', an employee walking to the right; 
+'<', an employee walking to the left; and '-', an empty space. Every employee walks at the same speed either 
+to right or to the left, according to their direction. Whenever two employees cross, each of them salutes the other. 
+They then continue walking until they reach the end, finally leaving the hallway. In the above example, they salute 10 times.
+
+Write a function solution(s) which takes a string representing employees walking along a hallway and returns the number 
+of times the employees will salute. s will contain at least 1 and at most 100 characters, each one of -, >, or <.
+    '''
+
+
+    #It's really a matter of calculating combinations of >< in order
+    #A simple way could be to loop through the list and count left facing and right facing in order
+    #We would want to restart this count each time we come to a < character
+    
+    
+    salutes = 0
+    
+    r = 0
+    for i in string:
+        if i == ">":
+            r += 1
+        elif i == "-":
+            continue
+        elif r == 0:
+            string.replace(i, '-', 1)
+        else:
+            salutes += r*2
+    
+    return salutes
+
+    
